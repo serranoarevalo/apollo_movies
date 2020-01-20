e5c06cde07d3ca04e8c03409e3dd7bd5d8ca2e7e
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2020-01-20 18:29:58 +0900

    Updates

---
+++ precache-manifest.79df9bd269aece960bb7a198e23949fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a06185ca3206473dbf766c15c9084a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0d37a266bf004ab2f77",
+    "url": "/static/css/2.1c2325a6.chunk.css"
+  },
+  {
+    "revision": "c0d37a266bf004ab2f77",
+    "url": "/static/js/2.ee5d1634.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/static/js/2.ee5d1634.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b36950db0da687a6c0f2",
+    "url": "/static/js/main.ef98ad77.chunk.js"
+  },
+  {
+    "revision": "a5fff528f104e7bd70c8",
+    "url": "/static/js/runtime-main.9f17d9f4.js"
+  }
+]);
